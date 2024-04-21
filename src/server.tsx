7e0f4d614bdb2216c2a4bccefe0b0463769de163
Yantao Wang <yantaowang@192.168.0.101>
2024-04-21 17:57:31 +0800

    tx to tsx

--- src/server.tsx
+++ src/server.tsx
@@ -1,6 +1,17 @@
+// Import Libraries used later.
+
+
 import { Hono } from 'hono'
 
+
+
+
 const app = new Hono()
+
+// The first stage is to make the steklons.
+// Use JSX 
+
+
 
 app.get('/', (c) => {
   return c.text('Hello Hono!')
